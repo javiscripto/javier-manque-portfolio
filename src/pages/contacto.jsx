c326aaa355daf6ed10a9a74b9d9e442c8f1c0e6d
javiscripto <javier.mecker94@gmail.com>
2024-11-25 22:47:59 -0300

    modificacion de formulario de contacto

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -2,24 +2,81 @@ import React, { useState } from 'react'
 import { ScrollRevealEffect } from '../components/efects/ScrollrevealEffect'
 import styles from "./contacto.module.css";
 
+
+
 export default function Contacto() {
-  const [input, setInput] = useState('');
+
+  const [input, setInput] = useState({
+    nombre: "",
+    correo: "",
+    mensaje: ""
+  });
+
+  const [loading, setLoading] = useState(false);
+
 
   const handleInput = (e) => {
-    setInput(e.target.value)
-    console.log(input);
+    const { name, value } = e.target;
+
+    setInput({
+      ...input,
+      [name]: value
+    })
+
   }
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("datos recibidos: ", input)
+    try {
+      setLoading(true);
+      const response = await fetch('http://localhost:3000/send-email', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(input)
+      });
+
+      if (!response.ok) {
+        throw new Error("ha ocurrido un error al enviar el formulario");
+      }
+      const data = await response.json();
+      console.log("data: ", data);
+      alert("mensaje enviado con exito");
+
+      setInput({
+        nombre: "",
+        correo: "",
+        mensaje: ""
+      })
+    } catch (error) {
+      console.error("ha ocurrido un error al enviar el formulario: ", error)
+    } finally {
+      setLoading(false);
+    }
   }
   return (
     <ScrollRevealEffect>
       <div className='container'>
         <h2>Contacto</h2>
+        {loading && <h3 style={{ color: "red" }}>Enviando...</h3>}
         <div className={styles.form}>
           <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Nombre' onChange={handleInput} />
+
+            <div className={styles.field}>
+              <label>Nombre</label>
+              <input type="text" placeholder='John Perez' name="nombre" onChange={handleInput} />
+            </div>
+
+            <div className={styles.field}>
+              <label>Email</label>
+              <input type="email" placeholder='example@mail.com' name="correo" onChange={handleInput} />
+            </div>
+
+            <div className={styles.field}>
+              <label>Mensaje</label>
+              <textarea placeholder='Deja aquí tu mensaje, con gusto lo leeré' name="mensaje" onChange={handleInput} />
+            </div>
+
             <button type='submit'>enviar</button>
           </form>
         </div>
