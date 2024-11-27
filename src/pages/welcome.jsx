595a565b268a87932e854643efa77d9cc058444a
javiscripto <javier.mecker94@gmail.com>
2024-11-26 22:21:14 -0300

    reglas media  y animacion en pagina de inicio

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -2,30 +2,24 @@ import React from 'react'
 import { motion } from 'motion/react';
 import { Canvas } from '../components/canvas/Canvas'
 import styles from './wlcome.module.css';
+import { ScrollRevealEffect } from '../components/efects/ScrollrevealEffect';
 export const Welcome = () => {
   return (
     <div className='container'>
 
-
       <div className={styles.gridContainer}>
-        <motion.div initial={{ opacity: 0 }} animate={{
-          transition: { duration: .5 },
-          opacity: 1
-        }}>
-          <Canvas className={styles.gridCanvas} />
-        </motion.div>
+
+        <Canvas className={styles.gridCanvas} />
         <div className={styles.gridText}>
           <h3 className='title'>Javier Manque - fullstack web developer</h3>
-          <motion.div initial={{ opacity: 0 }} animate={{
-            opacity: 1,
-            transition: { duration: .8 }
-          }}>
+
+          <ScrollRevealEffect>
             <p>
               Soy desarollador web fullstack viviendo actualmente en la ciudad de Valdivia, Chile.
               Tengo conocimientos en bases de datos SQL y noSQL (mySQL, SQLite y mongoDB); en los lenguajes de programacion Javascript y python y manejo frameworks de estos lenguajes tanto para el frontend como para el backend.
             </p>
 
-          </motion.div>
+          </ScrollRevealEffect>
         </div>
 
       </div>
