b54d4f2ef03b290d97171b3c84d084ef204f0cfc
javier ismael <javier.mecker94@gmail.com>
2025-06-17 12:53:46 -0400

    estilos en cards en formacion y proyectos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,24 @@
-import React from 'react'
-import styles from './Card.module.css';
-
+import React from "react";
+import styles from "./Card.module.css";
 
 export default function Card({ course, handleToggleVisibility }) {
-  const { title, description, img } = course
+  const { title, description, img } = course;
 
   return (
-      <div className={styles.cardContainer} >
-
-        <div
-          className={styles.cardImg}
-          onClick={() => handleToggleVisibility(img, true)}>
-
-
-          <img src={img} alt={title} />
-        </div>
-        <div className={styles.textContainer}>
-          <h3 className="title">{title}</h3>
-          <p>
-            {description}
-          </p>
-
-        </div>
+    <div className={styles.cardContainer}>
+      <div
+        className={styles.cardImg}
+        onClick={() => handleToggleVisibility(img, true)}
+      >
+        <img src={img} alt={title} />
       </div>
-
-  )
+      {/* <div className={styles.textContainer}> */}
+      {/*   <h3 className="title">{title}</h3> */}
+      {/*   <p> */}
+      {/*     {description} */}
+      {/*   </p> */}
+      {/**/}
+      {/* </div> */}
+    </div>
+  );
 }
-
